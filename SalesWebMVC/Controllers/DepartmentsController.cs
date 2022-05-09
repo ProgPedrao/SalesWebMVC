97bc4c0414795ebdc154f9b75be7390d478884c8
Pedro Henrique Ramos Martins <pedrohrmartins98@gmail.com>
2022-05-08 23:15:58 -0300

    First Model-Controller-View - Department

---
+++ SalesWebMVC/Controllers/DepartmentsController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using SalesWebMVC.Models;
+
+namespace SalesWebMVC.Controllers
+{
+    public class DepartmentsController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<Department> departments = new List<Department>();
+
+            departments.Add(new Department() { Id = 1, Name = "Pedro" });
+            departments.Add(new Department() { Id = 2, Name = "Radassa" });
+            departments.Add(new Department() { Id = 3, Name = "nelio" });
+
+            return View(departments);
+        }
+    }
+}
